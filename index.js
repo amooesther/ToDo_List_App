dd32a94c9a7590098e81c41e477b87ace7b6eb11
Esther Toyin <amooesther2503@gmail.com>
2023-12-18 10:06:30 +0000

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 function addTask() {
     const taskInput = document.getElementById("taskInput");
     const taskList = document.getElementById("taskList");
@@ -7,13 +5,19 @@ function addTask() {
     if (taskInput.value.trim() !== "") {
         const newTask = document.createElement("li");
         newTask.innerHTML = `
-            ${taskInput.value}
+            <span onclick="toggleComplete(this)">${taskInput.value}</span>
             <button class="remove-button" onclick="removeTask(this)">Remove</button>
         `;
         taskList.appendChild(newTask);
         taskInput.value = "";
     }
 }
+
+function toggleComplete(taskSpan) {
+  
+    taskSpan.parentNode.classList.toggle("completed");
+}
+
 
 function removeTask(button) {
     const taskList = document.getElementById("taskList");
